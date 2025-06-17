42104cebd2b2283cb22432db75fffbbd9bfc8e29
Arthur Broudoux <arthur.broudoux@gmail.com>
2025-06-17 10:56:35 +0200

    feat: compareAndFindCommonFilms method Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- main.go
+++ main.go
@@ -19,6 +19,10 @@ type LetterboxdWatchlistFilm struct {
 	PosterUrl string `json:"poster_url"`
 }
 
+type LetteboxdWatchlist struct {
+	Films []LetterboxdWatchlistFilm `json:"films"`
+}
+
 func setupRouter() *gin.Engine {
 	router := gin.Default()
 
@@ -28,7 +32,7 @@ func setupRouter() *gin.Engine {
 
 	router.GET("/users/:usernames", func(context *gin.Context) {
 		usernamesQuery := context.Param("usernames")
-		usernames := strings.Split(usernamesQuery, ";")
+		usernames := strings.Split(usernamesQuery, ",")
 
 		results := fetchScrapper(usernames)
 
@@ -40,7 +44,7 @@ func setupRouter() *gin.Engine {
 
 func fetchScrapper(usernames []string) []LetterboxdWatchlistFilm {
 	var wg sync.WaitGroup
-	resultChan := make(chan []LetterboxdWatchlistFilm, len(usernames))
+	resultChan := make(chan LetteboxdWatchlist, len(usernames))
 
 	for _, username := range usernames {
 		wg.Add(1)
@@ -49,39 +53,73 @@ func fetchScrapper(usernames []string) []LetterboxdWatchlistFilm {
 			url := fmt.Sprintf("http://localhost:8000/api/watchlist/%s", u)
 			resp, err := http.Get(url)
 			if err != nil {
-				log.Error("Error for user %s: %v\n", u, err)
-				resultChan <- []LetterboxdWatchlistFilm{}
+				log.Errorf("Error for user %s: %v", u, err)
+				resultChan <- LetteboxdWatchlist{}
 				return
 			}
 			defer resp.Body.Close()
 
 			body, err := io.ReadAll(resp.Body)
 			if err != nil {
-				log.Error("Error during body reading for user %s: %v\n", u, err)
-				resultChan <- []LetterboxdWatchlistFilm{}
+				log.Errorf("Error during body reading for user %s: %v", u, err)
+				resultChan <- LetteboxdWatchlist{}
 				return
 			}
 
 			var data []LetterboxdWatchlistFilm
 			if err := json.Unmarshal(body, &data); err != nil {
-				log.Error("Error during JSON parsing for user %s: %v\n", u, err)
-				resultChan <- []LetterboxdWatchlistFilm{}
+				log.Errorf("Error during JSON parsing for user %s: %v", u, err)
+				resultChan <- LetteboxdWatchlist{}
 				return
 			}
 
-			resultChan <- data
+			for i := range data {
+				data[i].Author = u
+			}
+
+			resultChan <- LetteboxdWatchlist{Films: data}
 		}(username)
 	}
 
 	wg.Wait()
 	close(resultChan)
 
-	finalResults := []LetterboxdWatchlistFilm{}
-	for r := range resultChan {
-		finalResults = append(finalResults, r...)
+	allWatchlists := []LetteboxdWatchlist{}
+	for wl := range resultChan {
+		allWatchlists = append(allWatchlists, wl)
 	}
 
-	return finalResults
+	return compareAndFindCommonFilms(allWatchlists)
+}
+
+func compareAndFindCommonFilms(watchlists []LetteboxdWatchlist) []LetterboxdWatchlistFilm {
+	if len(watchlists) == 0 {
+		return nil
+	}
+
+	commonFilms := make(map[string]LetterboxdWatchlistFilm)
+	for _, film := range watchlists[0].Films {
+		key := fmt.Sprintf("%s-%d", film.Title, film.Year)
+		commonFilms[key] = film
+	}
+
+	for _, watchlist := range watchlists[1:] {
+		currentFilms := make(map[string]LetterboxdWatchlistFilm)
+		for _, film := range watchlist.Films {
+			key := fmt.Sprintf("%s-%d", film.Title, film.Year)
+			if _, exists := commonFilms[key]; exists {
+				currentFilms[key] = film
+			}
+		}
+		commonFilms = currentFilms
+	}
+
+	results := make([]LetterboxdWatchlistFilm, 0, len(commonFilms))
+	for _, film := range commonFilms {
+		results = append(results, film)
+	}
+
+	return results
 }
 
 func main() {
